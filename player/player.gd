08d1c4a1af7aebbe0c677dae6958c3cdc01c5ed6
Paraldos <hillbrecht.s@gmail.com>
2024-07-29 23:04:01 +0200

    reworked player state machine

--- player/player.gd
+++ player/player.gd
@@ -5,11 +5,12 @@ const JUMP_VELOCITY = -130.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var player_animation = $PlayerAnimation
 var direction = Input.get_axis("ui_left", "ui_right")
-var status = "Idle"
+var current_weapon = "Axe"
+var state = "Idle"
 
 func _physics_process(delta):
 	direction = Input.get_axis("ui_left", "ui_right")
-	status = "Idle"
+	state = "Idle"
 	_jump(delta)
 	_attack()
 	_movement()
@@ -17,33 +18,31 @@ func _physics_process(delta):
 	_friction(delta)
 	move_and_slide()
 
-func _friction(delta):
-	if not is_on_floor():
-		velocity.y += gravity * delta
-	if direction == 0:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-func _attack():
-	if status != "Idle":
-		return
-	if Input.is_action_pressed("ui_attack"):
-		status = "Bow"
-
 func _jump(delta):
+	if Input.is_action_just_pressed("ui_jump") or not is_on_floor():
+		state = "Jump"
 	if not is_on_floor():
-		status = "Jump"
 		if direction:
 			velocity.x = direction * SPEED
 	if Input.is_action_just_pressed("ui_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		status = "Jump"
+
+func _attack():
+	if state == "Jump": return
+	if player_animation.state_machine.get_current_node() == "Attack" or Input.is_action_pressed("ui_attack"):
+		state = "Attack"
 
 func _movement():
-	if status != "Idle":
-		return
+	if state != "Idle": return
 	if direction:
 		velocity.x = direction * SPEED
-		status = "Run"
+		state = "Run"
+
+func _friction(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	if state != "Run" and is_on_floor():
+		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 func _animation():
-	player_animation._change_animation(status, velocity)
+	player_animation._change_animation(state, velocity)
--- player/player_animation.gd
+++ player/player_animation.gd
@@ -3,18 +3,19 @@ extends Sprite2D
 @onready var animation_player = $AnimationPlayer
 @onready var animation_tree = $AnimationTree
 @onready var state_machine = animation_tree["parameters/playback"]
-var current_state = "idle"
 @onready var attack_bow = $AttackBow
 
 func _ready():
 	animation_tree.active = true
+	_change_weapon(get_parent().current_weapon)
 
 func _change_animation(new_state, direction):
 	_change_x_direction(direction.x)
 	_change_y_direction(direction.y)
-	if current_state == new_state: return
-	current_state = new_state
-	state_machine.travel(new_state)
+	if state_machine.get_current_node() == new_state:
+		return
+	else:
+		state_machine.travel(new_state)
 
 func _change_x_direction(x_dir):
 	if x_dir == 0: return
@@ -24,6 +25,11 @@ func _change_y_direction(y_dir):
 	if y_dir == 0: return
 	animation_tree.set("parameters/Jump/blend_position", y_dir / 50)
 
+func _change_weapon(new_weapon):
+	if new_weapon == "Bow":
+		animation_tree.set("parameters/Attack/blend_position", 1)
+	else:
+		animation_tree.set("parameters/Attack/blend_position", -1)
+
 func _emit_attack_signal():
 	attack_bow._attack(flip_h)
-	current_state = "Idle"
