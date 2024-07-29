9dc7317e2d4bd2d475b35eb7a08f433b0f37ddf3
Paraldos <hillbrecht.s@gmail.com>
2024-07-29 19:35:59 +0200

    reafactored player assets and animation

--- player/player.gd
+++ player/player.gd
@@ -1,38 +1,33 @@
 extends CharacterBody2D
 
-
-const SPEED = 100.0
-const JUMP_VELOCITY = -200.0
-@onready var animation_player = $AnimationPlayer
-@onready var sprite_2d = $Sprite2D
-@onready var weapons = $Weapons
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
+const SPEED = 80.0
+const JUMP_VELOCITY = -150.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+@onready var player_animation = $PlayerAnimation
 
 func _physics_process(delta):
-	# Add the gravity.
+	_jump(delta)
+	_movement()
+	_animation()
+	move_and_slide()
+
+func _jump(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
-	weapons.look_at(get_global_mouse_position())
-
-	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+func _movement():
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
-		animation_player.play("Walk")
 		velocity.x = direction * SPEED
-		if velocity.x > 0:
-			sprite_2d.flip_h = false
-		else:
-			sprite_2d.flip_h = true
 	else:
-		animation_player.play("Idle")
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	move_and_slide()
+func _animation():
+	if !is_on_floor():
+		player_animation._change_animation("Jump", velocity)
+	elif velocity.x != 0:
+		player_animation._change_animation("Run", velocity)
+	else:
+		player_animation._change_animation("Idle", velocity)
---
+++ player/player_animation.gd
@@ -0,0 +1,24 @@
+extends Sprite2D
+
+@onready var animation_player = $AnimationPlayer
+@onready var animation_tree = $AnimationTree
+@onready var state_machine = animation_tree["parameters/playback"]
+var current_state = "idle"
+
+func _ready():
+	animation_tree.active = true
+
+func _change_animation(new_state, direction):
+	_change_x_direction(direction.x)
+	_change_y_direction(direction.y)
+	if current_state == new_state: return
+	current_state = new_state
+	state_machine.travel(new_state)
+
+func _change_x_direction(x_dir):
+	if x_dir == 0: return
+	flip_h = x_dir <= 0
+
+func _change_y_direction(y_dir):
+	if y_dir == 0: return
+	animation_tree.set("parameters/Jump/blend_position", y_dir / 50)
