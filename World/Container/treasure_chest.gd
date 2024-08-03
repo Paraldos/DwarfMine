1011509074c7aa36855b05d9c587ad9425e8f1b9
Paraldos <hillbrecht.s@gmail.com>
2024-08-03 19:15:40 +0200

    refactored treasure spawner

--- World/Container/treasure_chest.gd
+++ World/Container/treasure_chest.gd
@@ -8,9 +8,6 @@ enum TreasureSize { SMALL, MEDIUM, BIG }
 @onready var collision_shape_hurtbox = %CollisionShapeHurtbox
 @onready var fx_spawner = $FXSpawner
 
-func _ready():
-	treasure_spawner.treasure_size = treasure_size
-
 func _on_hurt_box_hit(dmg):
 	if sprite_2d.frame == 0:
 		call_deferred("_deferred_open")
@@ -22,7 +19,8 @@ func _deferred_open():
 	collision_shape_hurtbox.disabled = true
 	sprite_2d.frame += 1
 	interaction_area._change_status(false)
-	treasure_spawner._spawn_treasure()
+	for i in treasure_size:
+		treasure_spawner._spawn_treasure()
 	fx_spawner._spawn()
 
 func _on_interaction_area_in_focus():
--- components/TreasureSpawner.gd
+++ components/TreasureSpawner.gd
@@ -1,8 +1,6 @@
 extends Node
 class_name TreasureSpawner
 
-enum TreasureSize { SMALL, MEDIUM, BIG }
-@export var treasure_size: TreasureSize = TreasureSize.SMALL
 var pickup = preload("res://World/Pickup/pickup.tscn")
 var coin = preload("res://World/Pickup/coin.tscn")
 var rng = RandomNumberGenerator.new()
@@ -10,18 +8,16 @@ var rng = RandomNumberGenerator.new()
 func _spawn_treasure():
 	rng.randomize()
 	var treasure = []
-	for i in treasure_size +1:
-		if rng.randi_range(0, 100) < 60:
-			_spawn_element(coin.instantiate())
-		else:
-			_spawn_element(pickup.instantiate())
+	if rng.randi_range(0, 100) < 60:
+		_spawn_element(coin.instantiate())
+	else:
+		_spawn_element(pickup.instantiate())
 
 func _spawn_element(new_element):
 	new_element.global_position = get_parent().global_position
 	new_element.velocity.x = rng.randi_range(-150, 150)
 	new_element.velocity.y = rng.randi_range(-120, -80)
 	get_tree().current_scene.add_child(new_element)
-
 
 func _add_pickup_to_scene(new_element):
 	get_tree().current_scene.add_child(new_element)
