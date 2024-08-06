388772c0de7ea3cf9716d17ea5aa4d14f4b69f1b
Paraldos <hillbrecht.s@gmail.com>
2024-08-06 18:45:27 +0200

    fixed minor naming issues

--- World/level.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-@export var connected_rooms = {
-	Vector2(1,0): null,
-	Vector2(-1,0): null,
-	Vector2(0,1): null,
-	Vector2(0,-1): null
-}
---
+++ World/world.gd
@@ -0,0 +1,3 @@
+extends Node2D
+
+var dungeon_layout = 0
