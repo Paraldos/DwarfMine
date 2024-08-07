859269cd6ea98845e76a9424c46166cb3c584002
Paraldos <hillbrecht.s@gmail.com>
2024-08-07 19:02:13 +0200

    refactored randome room generator

--- Singeltons/dungeon_generator.gd
+++ Singeltons/dungeon_generator.gd
@@ -1,12 +1,20 @@
 extends Node2D
 
+var directions = [
+	Vector2(0, 1),
+	Vector2(-1, 0),
+	Vector2(0, -1),
+	Vector2(1, 0),
+]
+var unused_direcionts = []
 var map_size = Vector2(10, 10)
-var amount_of_rooms = 10
-var max_attempts = 1000
 var map = []
 var rng = RandomNumberGenerator.new()
 var start_room = Vector2()
 var rooms = []
+var lengt_of_corridors = 4
+var branches = 3
+var placed_rooms = 0
 
 func _ready():
 	rng.randomize()
@@ -14,14 +22,13 @@ func _ready():
 	_generate_map()
 
 func _generate_map():
-	_create_empty_map()
+	_empty_map()
 	_define_start_room()
-	if _fill_dungeon_with_rooms():
-		print("Map successfully generated.")
-	else:
-		print("Failed to place all rooms.")
+	for branch in branches:
+		_fill_dungeon_with_rooms()
+	_print_map()
 
-func _create_empty_map():
+func _empty_map():
 	map = []
 	for y in range(map_size.y):
 		var row = []
@@ -30,42 +37,54 @@ func _create_empty_map():
 		map.append(row)
 
 func _define_start_room():
-	start_room = _get_randome_position()
+	start_room = Vector2(map_size.x / 2, map_size.y / 2)
 	rooms.append(start_room)
 	map[start_room.y][start_room.x] = 2
 
+
 func _fill_dungeon_with_rooms():
-	var placed_rooms = amount_of_rooms - 1
-	var attempts = 0
-	while placed_rooms > 1:
-		attempts += 1
-		rooms.shuffle()
-		var room = rooms[0]
-		var neighbors = [
-			Vector2(room.x - 1, room.y),
-			Vector2(room.x + 1, room.y),
-			Vector2(room.x, room.y - 1),
-			Vector2(room.x, room.y + 1)
-		]
-		for neighbor in neighbors:
-			if neighbor.x < 0 or neighbor.x >= (map_size.x -1):
-				continue
-			if neighbor.y < 0 or neighbor.y >= (map_size.y -1):
-				continue
-			if map[neighbor.y][neighbor.x] == 0:
-				map[neighbor.y][neighbor.x] = 1
-				rooms.append(neighbor)
-				placed_rooms -= 1
-				attempts = 0
-				print(placed_rooms)
-	return placed_rooms == 0
+	var favorit_direction = _get_favorite_direction()
+	rooms.shuffle()
+	var current_room = rooms[0]
+	for i in lengt_of_corridors:
+		var valid_neighbours = _get_valid_neighbour(current_room)
+		if valid_neighbours.size() == 0:
+			return false
+		else:
+			var next_room : Vector2
+			if valid_neighbours.has(current_room + favorit_direction):
+				next_room = current_room + favorit_direction
+			else:
+				next_room = valid_neighbours[rng.randi_range(0, valid_neighbours.size() - 1)]
+			map[next_room.y][next_room.x] = 1
+			rooms.append(next_room)
+			current_room = next_room
+			placed_rooms += 1
 
 func _print_map():
 	for i in map:
 		print(i)
+	print(placed_rooms)
 
 # ========================================================== Helper functions
-func _get_randome_position():
-	var randi_x = rng.randi_range(0, (map_size.x - 1))
-	var randi_y = rng.randi_range(0, (map_size.y - 1))
-	return Vector2(randi_x, randi_y)
+func _get_favorite_direction():
+	if unused_direcionts.size() == 0:
+		unused_direcionts = directions
+	unused_direcionts.shuffle()
+	return unused_direcionts[0]
+
+func _get_valid_neighbour(current_room):
+	var neighbours = []
+	var x = current_room.x
+	var y = current_room.y
+	var neighbors = [
+		Vector2(x - 1, y),
+		Vector2(x + 1, y),
+		Vector2(x, y - 1),
+		Vector2(x, y + 1)
+	]
+	for neighbour in neighbors:
+		if neighbour.x >= 0 and neighbour.x < map_size.x and neighbour.y >= 0 and neighbour.y < map_size.y:
+			if map[neighbour.y][neighbour.x] == 0:
+				neighbours.append(neighbour)
+	return neighbours
