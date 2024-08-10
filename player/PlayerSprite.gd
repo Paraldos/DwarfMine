1d8e1cb828ad04d13f4175a4942e67c48cbdff35
Paraldos <hillbrecht.s@gmail.com>
2024-08-10 10:05:01 +0200

    reworked animations

--- player/PlayerSprite.gd
+++ player/PlayerSprite.gd
@@ -1,18 +1,33 @@
-extends Sprite2D
+extends Node2D
 
 signal attack_animation_finished
 @onready var animation_player = $AnimationPlayer
 @onready var animation_tree = $AnimationTree
 @onready var state_machine = animation_tree["parameters/playback"]
+@onready var hitbox = %Hitbox
+@onready var muzzle = %Muzzle
+
+var facing_left = false
 
 func _ready():
 	animation_tree.active = true
 
 func _change_animation(new_animation):
+	for child in get_children():
+		if child is Sprite2D:
+			child.visible = child.name == new_animation
 	state_machine.travel(new_animation)
 
-func _set_direction(velocity):
+func _set_direction(velocity, delta):
 	animation_tree.set("parameters/Jump/blend_position", velocity.y / 50)
+	if velocity.x != 0:
+		facing_left = velocity.x <= 0
+	for child in get_children():
+		if child is Sprite2D:
+			child.flip_h = facing_left
+	for child in [hitbox, muzzle]:
+		child.scale.x = -1 if (facing_left) else 1
+	return facing_left
 
 func _attack_finished():
 	attack_animation_finished.emit()
--- player/player.gd
+++ player/player.gd
@@ -4,7 +4,6 @@ extends CharacterBody2D
 @export var JUMP_POWER = -140.0
 @onready var state_machine = $StateMachine
 @onready var player_sprite = %PlayerSprite
-@onready var player_direction = %PlayerDirection
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var facing_left = false
 
@@ -14,7 +13,7 @@ func _ready():
 
 func _physics_process(delta):
 	state_machine._state_physics_process(delta)
-	facing_left = player_direction._set_direction(delta, velocity)
+	facing_left = player_sprite._set_direction(velocity, delta)
 	move_and_slide()
 	if Input.is_action_just_pressed("switch_set"):
 		InventoryController._switch_set()
