669fdae8db4e40a6ce8c4015267d9bd7b37dc2f7
Paraldos <hillbrecht.s@gmail.com>
2024-08-10 11:20:28 +0200

    WIP: Ladders

---
+++ World/Scatter/ladder.gd
@@ -0,0 +1,8 @@
+extends TextureRect
+
+@onready var collision_shape_2d = $Area2D/CollisionShape2D
+
+func _ready():
+	collision_shape_2d.shape = collision_shape_2d.shape.duplicate()
+	collision_shape_2d.position = size /2
+	collision_shape_2d.shape.size = size
