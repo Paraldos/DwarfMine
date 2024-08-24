af790373eb4e7de24f955bc52b27a26f9dfd865e
Paraldos <hillbrecht.s@gmail.com>
2024-08-24 14:16:41 +0200

    reworked player and portal spawn logic

--- World/room_template.gd
+++ World/room_template.gd
@@ -2,10 +2,19 @@ extends Node2D
 
 @onready var player_position_marker = %PlayerPositionMarker
 var world = null
+var player_template = preload("res://player/player.tscn")
+var portal_template = preload("res://world/Objects/portal.tscn")
 
 func _spawn_player():
-	world.player.global_position = player_position_marker.global_position
-	get_tree().current_scene.add_child(world.player)
+	var player = player_template.instantiate()
+	player.global_position = player_position_marker.global_position
+	world.player = player
+	get_tree().current_scene.add_child(player)
+
+func _spawn_portal():
+	var portal = portal_template.instantiate()
+	portal.global_position = player_position_marker.global_position
+	get_tree().current_scene.add_child(portal)
 
 func _on_area_2d_body_entered(body):
 	world._move_camera(global_position)
--- World/world.gd
+++ World/world.gd
@@ -3,7 +3,6 @@ extends Node2D
 @onready var rooms = %Rooms
 @onready var camera = $Camera
 @onready var map = MapGenerator.map
-var player_template = preload("res://player/player.tscn")
 var room_size = Vector2(144, 80)
 var start_room = null
 var player = null
@@ -12,9 +11,12 @@ func _ready():
 	start_room = _spawn_room(MapGenerator.start_position)
 	for y in map.size():
 		for x in map[y].size():
-			if map[y][x] == 1 or map[y][x] == 3:
-				await _spawn_room(Vector2(x, y))
-	_spawn_player()
+			if map[y][x] != 0:
+				var room = _spawn_room(Vector2(x, y))
+				if map[y][x] == 2:
+					room._spawn_player()
+				if map[y][x] == 3:
+					room._spawn_portal()
 	Utils.mini_map__create.emit()
 
 func _move_camera(new_pos : Vector2, tween_position = true):
@@ -35,7 +37,3 @@ func _spawn_room(room_position):
 	new_room.world = self
 	rooms.add_child(new_room)
 	return new_room
-
-func _spawn_player():
-	player = player_template.instantiate()
-	start_room._spawn_player()
